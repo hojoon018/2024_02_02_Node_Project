08447ede851aa37bc0c53b0c55ad77a8b6590b8e
hojoon018 <skewed018@gmail.com>
2024-09-09 15:48:06 +0900

    [Feat] Node.js와 Unity3D WEB통신 Get까지

---
+++ Node/Main.js
@@ -0,0 +1,18 @@
+let express = require('express');                       //express 모듈을 가져온다.
+let app = express();                                    //express를 App 이름으로 정의하고 사용한다.
+
+app.get('/', function(req, res){                        //기본 라우터에서 Hello world를 반환한다.
+    res.send('Hello World');
+});
+
+app.get('/about', function(req, res){
+    res.send('about World');
+});
+
+app.get('/itemData', function(req, res){
+    res.send('itemData World');
+});
+
+app.listen(3000, function(){                            //3000포트에서 입력을 대기 한다.
+    console.log('Example app listening on port 3000');
+});
